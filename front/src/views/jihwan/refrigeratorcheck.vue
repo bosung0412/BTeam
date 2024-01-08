ef30b9d6e0c00be55508b96d8881e8a0f9a95f00
oraman <jeongas8130@naver.com>
2024-01-08 17:41:24 +0900

    상하단바추가

--- front/src/views/jihwan/refrigeratorcheck.vue
+++ front/src/views/jihwan/refrigeratorcheck.vue
@@ -1,20 +1,7 @@
 <template>
   <div id="app">
     <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-      <router-link to="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-        <img src="img/samplelogo.png" style="width: 70px; height: 70px; top: 0; left: 10px;" />
-      </router-link>
-      <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto p-4 p-lg-0">
-          <router-link to="/calendar" class="nav-item nav-link">캘린더</router-link>
-          <router-link to="/board" class="nav-item nav-link">게시판</router-link>
-        </div>
-      </div>
-    </nav>
+      <Navbar /> 
     <!-- Navbar End -->
 
     <!-- Page Header Start -->
@@ -55,29 +42,19 @@
       </div>
     </div>
     <!-- Copyright Start -->
-    <div class="container-fluid copyright py-4">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-            &copy; <a class="border-bottom" href="#">냉장고든램지</a>, All Right Reserved.
-          </div>
-          <div class="col-md-6 text-center text-md-end">
-            <!-- You can keep the credit link here if required -->
-          </div>
-        </div>
-      </div>
-    </div>
+    <Footer />
     <!-- Copyright End -->
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar/Navbar.vue';
+import Footer from '@/components/Footer/Footer.vue';
 export default {
-  // Add your component logic here
-}
-</script>
-
-<style scoped>
-/* Add your component-specific styles here */
-</style>
+  components: {
+    Navbar,
+    Footer,
+  }
+};
+  </script>
     
