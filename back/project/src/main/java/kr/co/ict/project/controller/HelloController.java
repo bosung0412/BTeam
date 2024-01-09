1b92473b960bf36a9424fb44e34679defc763ecd
mymysangmin <mymysangmin@naver.com>
2024-01-09 10:56:24 +0900

    back 추가

---
+++ back/project/src/main/java/kr/co/ict/project/controller/HelloController.java
@@ -0,0 +1,14 @@
+package kr.co.ict.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "안녕하세요 나의 첫 스프링 부트입니다.";
+
+    }
+}
