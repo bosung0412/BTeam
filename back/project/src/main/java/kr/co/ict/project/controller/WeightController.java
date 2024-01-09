d0c2626ab639f677e5f156a88d510d0929cd0b5e
mymysangmin <mymysangmin@naver.com>
2024-01-09 13:19:40 +0900

    back weight

---
+++ back/project/src/main/java/kr/co/ict/project/controller/WeightController.java
@@ -0,0 +1,21 @@
+package kr.co.ict.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.ict.project.service.WeightService;
+import kr.co.ict.project.vo.WeightVO;
+
+@RestController // Json형태로 객체 데이터 반환(controller + responseBody)
+public class WeightController {
+    @Autowired
+    private WeightService service;
+
+    @GetMapping("/weightList")
+    public List<WeightVO> listweight() {
+        return service.find();
+    }
+}
---
+++ back/project/src/main/java/kr/co/ict/project/vo/WeightVO.java
@@ -0,0 +1,16 @@
+package kr.co.ict.project.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Alias("wvo")
+@Getter
+@Setter
+public class WeightVO {
+    private String id; // 회원아이디
+    private int pastweight; // 과거몸무게
+    private int currentweight; // 현재 몸무게
+    private int futureweight; // 목표 몸무게
+}
