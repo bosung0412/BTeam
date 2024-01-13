eee4cf54a2478b70a51f3a58e0b9834b16858861
oraman <jeongas8130@naver.com>
2024-01-13 18:29:18 +0900

    1

--- back/project/src/main/java/kr/co/ict/project/ProjectApplication.java
+++ back/project/src/main/java/kr/co/ict/project/ProjectApplication.java
@@ -2,10 +2,7 @@ package kr.co.ict.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kr.co.ict.project.config.WebConfig;
 
@@ -16,19 +13,4 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer crosConfigure() {
-
-		return new WebMvcConfigurer() {
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				System.out.println("Test==========");
-				registry.addMapping("/**")
-						.allowedOrigins("*", "http://localhost:8080/", "192.0.0.2")
-						.allowedHeaders("*")
-						.allowedMethods("*").maxAge(3600);
-			}
-		};
-	}
 }
