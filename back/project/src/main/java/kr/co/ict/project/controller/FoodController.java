7df8ae1a58d1fde6e8511cf6e5b3a6e0b38edd45
zigu123 <wlghks1535@naver.com>
2024-01-16 16:08:37 +0900

    tlqkf wrkxsp

--- back/project/src/main/java/kr/co/ict/project/controller/FoodController.java
+++ back/project/src/main/java/kr/co/ict/project/controller/FoodController.java
@@ -1,6 +1,7 @@
 package kr.co.ict.project.controller;
 
 import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.file.Paths;
 import java.util.List;
@@ -36,32 +37,38 @@ public class FoodController {
 
     // 식단 상세 정보
     @GetMapping(value = "/detail")
-    public FoodVO getFoodDetail(@RequestParam("fno") int fno) {
-        FoodVO vo = service.FoodDetail(fno);
+    public FoodVO getFoodDetail(@RequestParam("nutrient_id") int nutrient_id) {
+        FoodVO vo = service.FoodDetail(nutrient_id);
         return vo;
     }
     
 
     @PostMapping(value = "/foodadd", produces = "application/json; charset=UTF-8")
-    public ResponseEntity<?> handleFileUpload(@RequestParam("fname") String fname,
-                                                @RequestParam("fprovince") String fprovince,
-                                                @RequestParam("ffprovince") String ffprovince,
-                                                @RequestParam("fcholesterol") String fcholesterol,
-                                                @RequestParam("fsalt") String fsalt,
-                                                @RequestParam("fpotasium") String fpotasium,
-                                                @RequestParam("fcarbohydrate") String fcarbohydrate,
-                                                @RequestParam("fdietaryfiber") String fdietaryfiber,
-                                                @RequestParam("fprotein") String fprotein,
-                                                @RequestParam("tocal") String tocal,
-                                                @RequestParam("fimage_file_name") List<MultipartFile> files) {
+    public ResponseEntity<?> handleFileUpload(@RequestParam("name") String name,
+                                                @RequestParam("weight") int weight,
+                                                @RequestParam("cal") int cal,
+                                                @RequestParam("carbo") int carbo,
+                                                @RequestParam("sugars") int sugars,
+                                                @RequestParam("fat") int fat,
+                                                @RequestParam("protein") int protein,
+                                                @RequestParam("calcium") int calcium,
+                                                @RequestParam("phosphorus") int phosphorus,
+                                                @RequestParam("sodium") int sodium,
+                                                @RequestParam("potassium") int potassium,
+                                                @RequestParam("magnesium") int magnesium,
+                                                @RequestParam("iron") int iron,
+                                                @RequestParam("zinc") int zinc,
+                                                @RequestParam("cholesterol") int cholesterol,
+                                                @RequestParam("transfat") int transfat,
+                                                @RequestParam("food_img") List<MultipartFile> files) {
 
         System.out.println("Code");
-        String fimage_file_name = null;
+        String food_img = null;
         for (MultipartFile multipartFile : files) {
             String originalFileName = multipartFile.getOriginalFilename();
             String extension = getExtension(originalFileName);
-            fimage_file_name = UUID.randomUUID().toString() + extension;
-            String filePath = imageDirectory + fimage_file_name;
+            food_img = UUID.randomUUID().toString() + extension;
+            String filePath = imageDirectory + food_img;
             try (FileOutputStream writer = new FileOutputStream(filePath)) {
                 writer.write(multipartFile.getBytes());
             } catch (Exception e) {
@@ -71,41 +78,61 @@ public class FoodController {
         }
 
         // URL 디코딩 수행
-        try {
-            fname = URLDecoder.decode(fname, "UTF-8");
-            fprovince = URLDecoder.decode(fprovince, "UTF-8");
-            ffprovince = URLDecoder.decode(ffprovince, "UTF-8");
-            fcholesterol = URLDecoder.decode(fcholesterol, "UTF-8");
-            fsalt = URLDecoder.decode(fsalt, "UTF-8");
-            fpotasium = URLDecoder.decode(fpotasium, "UTF-8");
-            fcarbohydrate = URLDecoder.decode(fcarbohydrate, "UTF-8");
-            fdietaryfiber = URLDecoder.decode(fdietaryfiber, "UTF-8");
-            fprotein = URLDecoder.decode(fprotein, "UTF-8");
-            tocal = URLDecoder.decode(tocal, "UTF-8");
+            try {
+        name = URLDecoder.decode(name, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    // 다른 변수들도 유사하게 처리
+    try {
+        weight = Integer.parseInt(URLDecoder.decode(String.valueOf(weight), "UTF-8"));
+        cal = Integer.parseInt(URLDecoder.decode(String.valueOf(cal), "UTF-8"));
+        carbo = Integer.parseInt(URLDecoder.decode(String.valueOf(carbo), "UTF-8"));
+        sugars = Integer.parseInt(URLDecoder.decode(String.valueOf(sugars), "UTF-8"));
+        fat = Integer.parseInt(URLDecoder.decode(String.valueOf(fat), "UTF-8"));
+        protein = Integer.parseInt(URLDecoder.decode(String.valueOf(protein), "UTF-8"));
+        calcium = Integer.parseInt(URLDecoder.decode(String.valueOf(calcium), "UTF-8"));
+        phosphorus = Integer.parseInt(URLDecoder.decode(String.valueOf(phosphorus), "UTF-8"));
+        sodium = Integer.parseInt(URLDecoder.decode(String.valueOf(sodium), "UTF-8"));
+        potassium = Integer.parseInt(URLDecoder.decode(String.valueOf(potassium), "UTF-8"));
+        magnesium = Integer.parseInt(URLDecoder.decode(String.valueOf(magnesium), "UTF-8"));
+        iron = Integer.parseInt(URLDecoder.decode(String.valueOf(iron), "UTF-8"));
+        zinc = Integer.parseInt(URLDecoder.decode(String.valueOf(zinc), "UTF-8"));
+        cholesterol = Integer.parseInt(URLDecoder.decode(String.valueOf(cholesterol), "UTF-8"));
+        transfat = Integer.parseInt(URLDecoder.decode(String.valueOf(transfat), "UTF-8"));
+    } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+    } catch (NumberFormatException e) {
+        e.printStackTrace();
+    }
 
-        System.out.println(fname);
+
+        System.out.println(name);
 
     
         FoodVO vo = new FoodVO();
-        vo.setFname(fname);
-        vo.setFprovince(fprovince);
-        vo.setFfprovince(ffprovince);
-        vo.setFcholesterol(fcholesterol);
-        vo.setFsalt(fsalt);
-        vo.setFpotasium(fpotasium);
-        vo.setFcarbohydrate(fcarbohydrate);
-        vo.setFdietaryfiber(fdietaryfiber);
-        vo.setFprotein(fprotein);
-        vo.setTocal(tocal);
-        vo.setFimage_file_name(fimage_file_name);
+        vo.setName(name);
+        vo.setWeight(weight);
+        vo.setCal(cal);
+        vo.setCarbo(carbo);
+        vo.setSugars(sugars);
+        vo.setFat(fat);
+        vo.setProtein(protein);
+        vo.setCalcium(calcium);
+        vo.setPhosphorus(phosphorus);
+        vo.setSodium(sodium);
+        vo.setPotassium(potassium);
+        vo.setMagnesium(magnesium);
+        vo.setIron(iron);
+        vo.setZinc(zinc);
+        vo.setCholesterol(cholesterol);
+        vo.setTransfat(transfat);
+        vo.setFood_img(food_img);
 
         service.insertFood(vo);
 
-        System.out.println("ImgName: " + fimage_file_name);
+        System.out.println("ImgName: " + food_img);
         return ResponseEntity.ok().body(vo);
     }
 
--- back/project/src/main/java/kr/co/ict/project/vo/FoodVO.java
+++ back/project/src/main/java/kr/co/ict/project/vo/FoodVO.java
@@ -9,16 +9,23 @@ import lombok.Setter;
 @Setter
 @Getter
 public class FoodVO {
-    private int fno;                // 음식 번호
-    private String fname;           // 음식 이름
-    private String fprovince;       // 지방
-    private String ffprovince;      // 포화 지방
-    private String fcholesterol;    // 콜레스테롤
-    private String fsalt;           // 나트륨
-    private String fpotasium;       // 칼륨
-    private String fcarbohydrate;   // 탄수화물
-    private String fdietaryfiber;   // 식이섬유
-    private String fprotein;        // 단백질
-    private String tocal;           // 총 칼로리
-    private String fimage_file_name; // 음식 사진
+    private int nutrient_id;      // 음식 영양소 아이디
+    private String name;           // 음식 이름
+    private int weight;       // 중량
+    private int cal;       // 에너지
+    private int carbo;       // 탄수화물
+    private int sugars;       // 당류
+    private int fat;       // 지방
+    private int protein;       // 단백질
+    private int calcium;       // 칼슘
+    private int phosphorus;       // 인
+    private int sodium;       // 나트륨
+    private int potassium;       // 칼륨
+    private int magnesium; // 마그네슘
+    private int iron; // 철
+    private int zinc; // 아연
+    private int cholesterol; // 콜레스테롤
+    private int transfat; // 트랜스지방
+    private String food_img; // 음식대표사진
+    
 }
