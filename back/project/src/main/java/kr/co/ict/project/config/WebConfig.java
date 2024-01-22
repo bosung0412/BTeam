3cfd2a926faa183e909d5ae4f119281437f1685d
bosung0412 <jeongas8130@naver.com>
2024-01-23 01:15:49 +0900

    123

--- back/project/src/main/java/kr/co/ict/project/config/WebConfig.java
+++ back/project/src/main/java/kr/co/ict/project/config/WebConfig.java
@@ -3,6 +3,7 @@ package kr.co.ict.project.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,10 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
    public WebMvcConfigurer crosConfigure() {
       return new WebMvcConfigurer() {
          @Override
+         
          public void addCorsMappings(CorsRegistry registry) {
             System.out.println("Test==========");
             registry.addMapping("/**")
-                  .allowedOrigins("http://192.168.0.230:8081/", "http://localhost:8081/", "http://192.168.0.230:8081/")
+                  .allowedOrigins("http://192.168.0.230:8081/", "http://localhost:8081/", "http://192.168.0.4:8081/")
                   .allowedHeaders("*")
                   .allowedMethods("*").maxAge(3600);
          }
--- front/vue.config.js
+++ front/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    port: 8081, // 사용하고자 하는 포트로 변경
+  },
+};
