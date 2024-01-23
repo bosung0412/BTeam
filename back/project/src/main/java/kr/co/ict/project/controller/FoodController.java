36750c766d3efef9fbbe64009df32dbba22d7355
oraman <wlghks1535@naver.com>
2024-01-23 14:54:30 +0900

    파트 끝

--- back/project/src/main/java/kr/co/ict/project/controller/FoodController.java
+++ back/project/src/main/java/kr/co/ict/project/controller/FoodController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import kr.co.ict.project.vo.FoodVO;
 
 
 @RestController
+@RequestMapping("/api/v1/auth")
 public class FoodController {
 
     @Autowired
